70ea74ada88b5d58af522385b64fef666304b861
alexandre596 <941836402>
2021-01-26 13:30:55 +0100

    Add spring boot main class

---
+++ src/main/java/br/com/alx/scrapper/AvailabilityScrapperBeApplication.java
@@ -0,0 +1,13 @@
+package br.com.alx.scrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvailabilityScrapperBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvailabilityScrapperBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alx/scrapper/AvailabilityScrapperBeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alx.scrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvailabilityScrapperBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
