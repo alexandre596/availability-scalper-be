f825d199fc2ce6c5a5275ca9a0417befe7ff13ce
alexandre596 <941836402>
2021-01-31 20:30:28 +0100

    Create profile configuration to load the correct phantom js based on operational system

--- src/main/java/br/com/alx/scrapper/configuration/GhostDriverConfiguration.java
+++ src/main/java/br/com/alx/scrapper/configuration/GhostDriverConfiguration.java
@@ -1,37 +1,41 @@
 package br.com.alx.scrapper.configuration;
 
+import br.com.alx.scrapper.exception.PhantomJsFileNotFoundException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
-import javax.annotation.PreDestroy;
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 public class GhostDriverConfiguration {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(GhostDriverConfiguration.class);
+
     @Bean(destroyMethod = "quit")
-    public WebDriver ghostDriver() {
+    public WebDriver ghostDriver(@Value("${phantom.js.file.location}") String phantomJsFileLocation) {
         try {
             //set binary path of phantomJS driver
             DesiredCapabilities capabilities = new DesiredCapabilities();
             capabilities.setJavascriptEnabled(true);
             //TODO read different files based on operation system using profiles
-            capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, new ClassPathResource("/lib/phantomjs-mac").getFile().getAbsolutePath());
+            capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, new ClassPathResource(phantomJsFileLocation).getFile().getAbsolutePath());
             capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_PAGE_SETTINGS_PREFIX, "Y");
-            capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, Arrays.asList("--webdriver-loglevel=NONE"));
+            capabilities.setCapability(PhantomJSDriverService.PHANTOMJS_CLI_ARGS, Collections.singletonList("--webdriver-loglevel=NONE"));
             capabilities.setCapability("phantomjs.page.settings.userAgent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:16.0) Gecko/20121026 Firefox/16.0");
 
             return new PhantomJSDriver(capabilities);
         } catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException(); //TODO throw a specific exception and notify
+            LOGGER.error("Could not load phantomjs file", e);
+            throw new PhantomJsFileNotFoundException(e);
         }
     }
 
